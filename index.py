80d14ce5226b69d054d5e2652ae46b6be5ed0123
홍은표 <44293278+potato179@users.noreply.github.com>
2022-10-17 09:47:24 +0900

    Update index.py

--- index.py
+++ index.py
@@ -21,5 +21,11 @@ for segment in SEGMENT_PINS:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, 0)
 
+# switch를 켰을 때  카메라 on 상태
+# 상시로 가변저항 값 읽고 7 segments에 0~9 단위로 띄우기
+# 7세그 값 하나 올라갈 때 마다 부저 삑소리 올라감
+# RGB LED 켜졌을 때는 초록색 꺼졌을 때는 빨간색
+# 잠깐만
+# 
 
 # https://rydepier.wordpress.com/2016/05/30/l9110-fan-motor-keyes-board/
