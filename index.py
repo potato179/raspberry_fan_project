9b86d23e962dca802a57749837ea2c66c05589b3
홍은표 <44293278+potato179@users.noreply.github.com>
2022-10-17 10:04:39 +0900

    Update index.py

--- index.py
+++ index.py
@@ -16,17 +16,22 @@ segment_pins = [15, 23, 24, 25, 21, 7, 12]
 fan_vcc_pin = 4
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
+GPIO.setup(buzzer_pin, GPIO.OUT)
+GPIO.setup(button_pin, GPIO.IN)
+GPIO.setup(switch_input_pin, GPIO.IN)
+GPIO.setup(led_red_pin, GPIO.OUT)
+GPIO.setup(led_green_pin, GPIO.OUT)
+GPIO.setup(led_blue_pin, GPIO.OUT)
 for segment in SEGMENT_PINS:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, 0)
-
+GPIO.setup(fan_vcc_pin, GPIO.OUT)
+GPIO.setwarnings(False)
 # switch를 켰을 때  카메라 on 상태
 # 상시로 가변저항 값 읽고 7 segments에 0~9 단위로 띄우기
 # 7세그 값 하나 올라갈 때 마다 부저 삑소리 올라감
 # RGB LED 켜졌을 때는 초록색 꺼졌을 때는 빨간색
 # 잠깐만
 # 버튼 누르면 소리하고 불빛 다 꺼 (야간모드)
-
+    
 # https://rydepier.wordpress.com/2016/05/30/l9110-fan-motor-keyes-board/
