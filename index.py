3c11e7a3c12b38249ce3220504437ffe61702488
홍은표 <44293278+potato179@users.noreply.github.com>
2022-10-24 10:48:15 +0900

    Update index.py

--- index.py
+++ index.py
@@ -3,31 +3,34 @@ import time
 
 # GPIO 7개 GPIO Pin 번호 설정
 #               A  B  C  D  E  F  G
-SEGMENT_PINS = [15, 23, 24, 25, 21, 7, 12]
+segment_pins = [15, 23, 24, 25, 13, 7, 12]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-for segment in SEGMENT_PINS:
+for segment in segment_pins:
     GPIO.setup(segment, GPIO.OUT)
     GPIO.output(segment, 0)
 
 # Common Anode일 경우 : LOW -> LED ON, HIGH -> LED OFF
 # Common Cathod일 경우 : LOW -> LED OFF, HIGH -> LED ON
 # data = [0, 0, 0, 0, 0, 0, 1]
-data = [1, 1, 1, 1, 1, 1, 0]
+data = [[1, 1, 1, 1, 1, 1, 0],  #0
+        [0, 1, 1, 0, 0, 0, 0],  #1
+        [1, 1, 0, 1, 1, 0, 1],  #2
+        [1, 1, 1, 1, 0, 0, 1],  #3
+        [0, 1, 1, 0, 0, 1, 1],  #4
+        [1, 0, 1, 1, 0, 1, 1],  #5
+        [1, 0, 1, 1, 1, 1, 1],  #6
+        [1, 1, 1, 0, 0, 0, 0],  #7
+        [1, 1, 1, 1, 1, 1, 1],  #8
+        [1, 1, 1, 0, 0, 1, 1]]  #9
 
 try:
-    for _ in range(3):
-        for i in range(len(SEGMENT_PINS)):
-            GPIO.output(SEGMENT_PINS[i], data[i])
-
-        time.sleep(1)
-
-        for i in range(len(SEGMENT_PINS)):
-            GPIO.output(SEGMENT_PINS[i], GPIO.LOW)
-
+    for i in range(10):
+        for j in range(len(segment_pins)):
+            GPIO.output(segment_pins[j], data[i][j])
         time.sleep(1)
 finally:
     GPIO.cleanup()
-    print('clean up and exit')
+    print("게이조이고")
