7f94aa6027525366287d9be288800a8cc4c03ac1
홍은표 <44293278+potato179@users.noreply.github.com>
2022-10-31 09:49:21 +0900

    Update index.py

--- index.py
+++ index.py
@@ -80,6 +80,8 @@ while True:
         pwm.stop()
         print("ok stop")
         GPIO.output(led_red_pin, GPIO.HIGH)
+        GPIO.output(led_green_pin, GPIO.LOW)
+        GPIO.output(led_blue_pin, GPIO.LOW)
     elif int(reading/103)*10 == 1:
         pwm.start(10)
         print("ok start")
